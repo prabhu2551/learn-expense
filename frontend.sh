a3688d2dc5f9b7e26d12229df5b963a45f0c8be8
prabhu2551 <deva246deva@gmail.com>
2024-02-22 08:39:45 +0530

    expense.frontend

--- frontend.sh
+++ frontend.sh
@@ -7,7 +7,7 @@ curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zi
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cp frontend.config /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf
 
 systemctl enable nginx
 systemctl restart nginx
