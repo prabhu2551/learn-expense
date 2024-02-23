9d9021a957e07dfb233f4c206a94cb5120de0b4e
prabhu2551 <deva246deva@gmail.com>
2024-02-23 06:51:35 +0530

    expense.frontend

--- backend.sh
+++ backend.sh
@@ -1,27 +1,41 @@
+echo -e "\e[32m disable nodejs \e[0m"
 dnf module disable nodejs -y
+
+echo -e "\e[32m enable nodejs \e[0m"
 dnf module enable nodejs:18 -y
 
+echo -e "\e[32m install nodejs \e[0m"
 dnf install nodejs -y
 
+echo -e "\e[32m copy backend.service file \e[0m"
 cp backend.service /etc/systemd/system/backend.service
 
+echo -e "\e[32m adding user \e[0m"
 useradd expense
 
+echo -e "\e[32m create app directory \e[0m"
 mkdir /app
 
+echo -e "\e[32m download backend.zip file \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 unzip /tmp/backend.zip
 
+echo -e "\e[32m app directory \e[0m"
 cd /app
 
+echo -e "\e[32m install dependencies \e[0m"
 npm install
 
+echo -e "\e[32m intall mysql \e[0m"
 dnf install mysql -y
 
+echo -e "\e[32m set uname and passwd \e[0m"
 mysql -h mysql-dev.vaddi.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
 
+echo -e "\e[32m  daemon-reload \e[0m"
 systemctl daemon-reload
 
+echo -e "\e[32m restart nginx \e[0m"
 systemctl enable backend
 systemctl restart backend
--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,15 @@
+echo -e "\e[33m installing nginx \e[0m"
 dnf module disable mysql -y
 
+echo -e "\e[33m installing nginx \e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 
+echo -e "\e[33m installing nginx \e[0m"
 dnf install mysql-community-server -y
 
+echo -e "\e[33m installing nginx \e[0m"
 systemctl enable mysqld
 systemctl restart mysqld
 
+echo -e "\e[33m installing nginx \e[0m"
 mysql_secure_installation --set-root-pass ExpenseApp@1
