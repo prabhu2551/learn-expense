0413bbcdae12f1325fba9c9ede075f85be1a64cc
prabhu2551 <deva246deva@gmail.com>
2024-02-23 06:34:06 +0530

    expense.frontend

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,19 @@
+echo -e "\e[35m installing nginx \e[0m"
 dnf install nginx -y
 
+echo -e "\e[35m cp expense.config file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf
 
+echo -e "\e[35m remove existing content \e[0m"
 rm -rf /usr/share/nginx/html/*
 
+echo -e "\e[35m download frontend content \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
+echo -e "\e[35m unzip the content \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-
+echo -e "\e[35m Restarting nginx \e[0m"
 systemctl enable nginx
 systemctl restart nginx
