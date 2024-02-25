6aeea7bb8fc40df116dd8c0040b5022d05643497
prabhu2551 <deva246deva@gmail.com>
2024-02-25 18:01:26 +0530

    expense.frontend

--- frontend.sh
+++ frontend.sh
@@ -22,7 +22,7 @@ cd /usr/share/nginx/html &>>log_file
 unzip /tmp/frontend.zip &>>log_file
 echo $?
 
-echo -e "${color}Restarting nginx \e[0m"
+echo -e "${color} Restarting nginx \e[0m"
 systemctl enable nginx &>>log_file
 systemctl restart nginx &>>log_file
 echo $?
